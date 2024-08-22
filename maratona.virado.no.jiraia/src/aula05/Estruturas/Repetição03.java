d5155cd9db5fb810a6abda35a31335470f7505d0
arthursant15 <157769865+arthursant15@users.noreply.github.com>
2024-08-21 22:01:31 -0300

    Estruturas de repetição 3

---
+++ maratona.virado.no.jiraia/src/aula05/Estruturas/Repetição03.java
@@ -0,0 +1,13 @@
+package aula05.Estruturas;
+
+public class Repetição03 {
+    public static void main(String[] args) {
+        for (int i = 1; i<=50;++i){
+            if (i>25){
+                break;
+            }
+            System.out.println(i);
+        }
+
+    }
+}
